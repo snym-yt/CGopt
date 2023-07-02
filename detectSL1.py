dd952fab44c8024f3e6b638ebe2cad0589d34287
daicha-w <58203483+daicha-w@users.noreply.github.com>
2023-07-02 13:04:38 +0900

    損失関数ごとに修正

--- detectSL1.py
+++ detectSL1.py
@@ -7,11 +7,8 @@ from matplotlib.image import imsave,imread
 from skimage.transform import resize
 import os,time,torch
 from torch import nn
-mse = nn.MSELoss()
-L1 = nn.L1Loss()
+
 SmoothL1 = nn.SmoothL1Loss()
-# Gaussian = nn.GaussianNLLLoss()
-Poisson = nn.PoissonNLLLoss()
 
 
 
@@ -182,29 +179,16 @@ class Dinonet:
             # ミニバッチ開始
             for i_batch,(x,y) in enumerate(dalo_train):
                 z = self.net(x.to(self.dev))
-                lossMSE = mse(z,y.to(self.dev)) # 訓練データの損失
-                lossL1 = L1(z,y.to(self.dev))
-                lossSL1 = SmoothL1(z,y.to(self.dev))
-                # lossGauss = Gaussian(z,y.to(self.dev))
-                lossPoisson = Poisson(z,y.to(self.dev))
+                loss = SmoothL1(z,y.to(self.dev)) # 訓練データの損失
                 self.opt.zero_grad()
-                lossMSE.backward()
-                lossL1.backward()
-                lossSL1.backward()
-                lossPoisson.backward()
+                loss.backward()
                 self.opt.step()
 
                 # 検証データにテスト
                 if((i_batch+1)%int(np.ceil(dalo_train.nkai/cnt_multi))==0 or i_batch==dalo_train.nkai-1):
                     self.net.eval()
-                    lossMSE = []
-                    lossL1 = []
-                    lossSL1 = []
-                    lossPoisson = []
-                    psnrMSE = []
-                    psnrL1 = []
-                    psnrSL1 = []
-                    psnrPoisson = []
+                    loss = []
+                    psnr = []
                     if(n_kaku):
                         gazou = []
                         n_kaita = 0
@@ -212,14 +196,8 @@ class Dinonet:
                     for x,y in test_data:
                         z = self.net(x.to(self.dev))
                         # 検証データの損失
-                        lossMSE.append(mse(z,y.to(self.dev)).item())
-                        lossL1.append(mse(z,y.to(self.dev)).item())
-                        lossSL1.append(mse(z,y.to(self.dev)).item())
-                        lossPoisson.append(mse(z,y.to(self.dev)).item())
-                        psnrMSE.append(10*np.log10((1^2)/mse(z,y.to(self.dev)).item()))
-                        psnrL1.append(10*np.log10((1^2)/L1(z,y.to(self.dev)).item()))
-                        psnrSL1.append(10*np.log10((1^2)/SmoothL1(z,y.to(self.dev)).item()))
-                        psnrPoisson.append(10*np.log10((1^2)/Poisson(z,y.to(self.dev)).item()))
+                        loss.append(SmoothL1(z,y.to(self.dev)).item())
+                        psnr.append(10*np.log10((1^2)/SmoothL1(z,y.to(self.dev)).item()))
                         # 検証データからできた一部の画像を書く
                         # if(n_kaita<n_kaku):
                         #     x = x.numpy().transpose(0,2,3,1) # 入力
@@ -231,14 +209,8 @@ class Dinonet:
                         #         n_kaita += 1
                         #         if(n_kaita>=n_kaku):
                         #             break
-                    lossMSE = np.mean(lossMSE)
-                    lossL1 = np.mean(lossL1)
-                    lossSL1 = np.mean(lossSL1)
-                    lossPoisson = np.mean(lossPoisson)
-                    psnrMSE = np.mean(psnrMSE)
-                    psnrL1 = np.mean(psnrL1)
-                    psnrSL1 = np.mean(psnrSL1)
-                    psnrPoisson = np.mean(psnrPoisson)
+                    loss = np.mean(loss)
+                    psnr = np.mean(psnr)
                     # if(n_kaku):
                     #     gazou = np.hstack(gazou)
                     #     # imsave(os.path.join(self.save_folder,'kekka%03d.jpg'%(kaime+1)),gazou)
@@ -255,10 +227,10 @@ class Dinonet:
             # sd = dict(w=self.net.state_dict(),o=self.opt.state_dict(),n=kaime+1,l=self.loss)
             # torch.save(sd,os.path.join(self.save_folder,'netparam.pkl'))
 
-            # 損失（MSE）の変化を表すグラフを書く
+            # 損失（SmoothL1）の変化を表すグラフを書く
             plt.figure(figsize=[5,4])
             plt.xlabel('trial')
-            plt.ylabel('MSE')
+            plt.ylabel('SmoothL1')
             ar = np.arange(1,kaime+2)
             plt.plot(ar,self.loss,'#11aa99')
             plt.tight_layout()
